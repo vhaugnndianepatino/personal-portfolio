ac7025a3bc76ce22fc8359ce2b4ccf4897ce8ed0
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2025-10-15 23:37:07 +0800

    Fix formspree

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -40,24 +40,18 @@ function Connect() {
           </div>
         </div>
         <div className="connect-right">
-          <form action="https://formspree.io/f/YOUR_FORMSPREE_ID" method="POST">
+          <form action="https://formspree.io/f/xeorvkvd" method="POST">
             <div className="form-group">
               <label htmlFor="name">Your Name</label>
-              <input type="text" id="name" name="name" required="" />
+              <input type="text" id="name" name="name" required />
             </div>
             <div className="form-group">
               <label htmlFor="email">Your Email</label>
-              <input type="email" id="email" name="email" required="" />
+              <input type="email" id="email" name="email" required />
             </div>
             <div className="form-group">
               <label htmlFor="message">Message</label>
-              <textarea
-                id="message"
-                name="message"
-                rows={5}
-                required=""
-                defaultValue={""}
-              />
+              <textarea id="message" name="message" rows="5" required></textarea>
             </div>
             <button type="submit" className="btn">
               Send Message <i className="fas fa-paper-plane" />
